c60cd9948097b3a5e5d773ed7f5caa1b9719b470
Adrien <adrien.cappelaere@gmail.com>
2019-10-14 20:34:49 +0200

    Ajout des exercices JS

---
+++ UE2.2-Javascript/script.js
@@ -0,0 +1,18 @@
+/* 
+    A partir de 2 nombres, effectuer les opérations +, -, * et / (vidéo)
+    Appliquez l'opérateur modulo (%) sur un nombre, opération qui retourne le reste de la division. Par ex. 5 % 2, retourne 1. En effet 5 ne se divise pas par 2, mais 2 * 2 = 4, il reste donc 1 pour obtenir 5 (vidéo)
+    D'un nombre n donné, calculez le carré, le cube. Pour le nombre 3, le carré vaut 3 * 3, le cube vaut 3 * 3 * 3, soit 9 et 27, respectivement. (vidéo)
+    Soit un rectangle de longueur L et de largeur l, calculez le périmètre, la surface. (vidéo)
+    D'un cercle de rayon R, calculez le diamètre, le périmètre, la surface. On prendra pour Pi la valeur 3.14, ou l'objet mathématique en javascript Math.PI (vidéo) (http://www.toutjavascript.com/reference/reference.php?ref=PI)
+    D'un montant HT, calculez la TVA (on prendra un taux de TVA de 20%), puis le montant TTC (vidéo)
+    Convertir un nombre de jours en heures, minutes, puis secondes. Par ex. 5 jours, c'est 120h, ou 7200mn ou encore 432000s.
+    Convertir un nombre de secondes en nombre de jours, d'heures, de minutes et de secondes. Utiliser l'opérateur modulo %. 1 jour, c'est 86400s. Information annexe : Typiquement on appelle un timestamp le nombre de secondes écoulées depuis le 1er janvier 1970 (langage PHP).
+*/
+
+//A partir de 2 nombres, effectuer les opérations +, -, * et / (vidéo)//
+
+var = x, y, z;
+x = 5;
+y = 12;
+z = x + y;
+
